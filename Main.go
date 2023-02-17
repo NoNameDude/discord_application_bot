dcd5cd53dce287359a5dde60f2ddce1e4172a4dc
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2023-02-17 23:58:38 +0100

    Create Main.go

---
+++ Main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var Token string = "Your token"
+var channel_id string = "Your channel id"
+
+func main() {
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var trigger bool = true
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if len(applications) > 0 {
+		for i := 0; i < len(applications); i++ {
+			fmt.Println(applications[i])
+		}
+	} else {
+		trigger = false
+	}
+	if trigger == false {
+		if m.ChannelID != channel_id {
+			fmt.Println(m.ChannelID, channel_id)
+			return
+		}
+	}
+	if m.Content == "!apply" {
+		channel, err := s.UserChannelCreate(m.Author.ID)
+		if err != nil {
+			fmt.Println("error creating channel:", err)
+			s.ChannelMessageSend(
+				m.ChannelID,
+				"Something went wrong while sending the DM!",
+			)
+			return
+		}
+		_, err = s.ChannelMessageSend(channel.ID, "Pong!")
+		if err != nil {
+			fmt.Println("error sending DM message:", err)
+			s.ChannelMessageSend(
+				m.ChannelID,
+				"Failed to send you a DM. Did you disable DM in your privacy settings?",
+			)
+		}
+	}
+}
